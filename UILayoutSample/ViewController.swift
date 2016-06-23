9b8fc033a3813bd69425e3e143217f9dda5c20cc
SeoDongHee <seoddong@hanmail.net>
2016-06-23 18:33:52 +0900

    Initial Commit

---
+++ UILayoutSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UILayoutSample
+//
+//  Created by SeoDongHee on 2016. 6. 23..
+//  Copyright © 2016년 SeoDongHee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
